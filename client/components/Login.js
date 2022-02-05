464740e025aef06f8e31d820495422f1eb7b4df7
Nicholas Shephard <shephardn@gmail.com>
2022-02-05 13:16:20 -0600

    Added components

---
+++ client/components/Login.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// import Pic from "../image/ProfilePicture.png"
+
+function Login() {
+ 
+
+  
+  return (
+    <div className="Login">
+      <h2>The Login Page</h2>
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/components/Signup.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// import Pic from "../image/ProfilePicture.png"
+
+function Employees() {
+ 
+
+  
+  return (
+    <div className="Employees">
+      <h2>The Employees Page</h2>
+    </div>
+  );
+}
+
+export default Employees;
