9e4c653bad142fd872599f9c4d45ba4ce5b13e6b
Nicholas Shephard <shephardn@gmail.com>
2022-02-08 13:17:50 -0600

    Added components

---
+++ client/src/components/ItemCard.js
@@ -0,0 +1,18 @@
+import '../style/App.css';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// import Pic from "../image/ProfilePicture.png"
+
+function ItemCard() {
+ 
+
+  
+  return (
+    <div className="ItemCard">
+      <h2>The ItemCard</h2>
+    </div>
+  );
+}
+
+export default ItemCard;
---
+++ client/src/components/JobForm.js
@@ -0,0 +1,18 @@
+import '../style/App.css';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// import Pic from "../image/ProfilePicture.png"
+
+function JobPage() {
+ 
+
+  
+  return (
+    <div className="JobPage">
+      <h2>The JobPage</h2>
+    </div>
+  );
+}
+
+export default JobPage;
