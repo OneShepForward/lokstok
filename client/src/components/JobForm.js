a74b5cd4b705ab267aa1fecb6755159fff490195
Nicholas Shephard <shephardn@gmail.com>
2022-02-09 05:46:27 -0600

    saved jobform

--- client/src/components/JobForm.js
+++ client/src/components/JobForm.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 // import Pic from "../image/ProfilePicture.png"
 
-function JobPage() {
+function JobForm() {
  
 
   
   return (
-    <div className="JobPage">
-      <h2>The JobPage</h2>
+    <div className="JobForm">
+      <h2>The JobForm</h2>
     </div>
   );
 }
 
-export default JobPage;
+export default JobForm;
